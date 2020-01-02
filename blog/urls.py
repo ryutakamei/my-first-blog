ccd6f8a6255301177e586ef059c2ecccd07b0bc8
Ryuta Kamei <ryutakamei06@gmail.com>
2020-01-03 03:39:14 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name='post_list'),    #サイトのデフォルトページをviews.pyのpost_listにリンク
+    path('', views.post_list, name='post_list'),    #サイトのデフォルトページをviews.pyのpost_listにリンク
+    path('post/<int:pk>/', views.post_detail, name='post_detail') #記事のurlの作成
 ]
