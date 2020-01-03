943d58784184852b3ba544d87799652525230e81
Ryuta Kamei <ryutakamei06@gmail.com>
2020-01-03 21:32:07 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),    #サイトのデフォルトページをviews.pyのpost_listにリンク
-    path('post/<int:pk>/', views.post_detail, name='post_detail') #記事のurlの作成
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #記事のurlの作成
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
